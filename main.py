dca0575ecd52c45fc01d14640387a67e055edddd
TheSlendy <artemmuksu@mail.ru>
2023-01-20 16:44:44 +0700

    Fixed AttributeError for face mesh processing

--- main.py
+++ main.py
@@ -11,17 +11,24 @@ cap = cv2.VideoCapture(0)
 
 while True:
     _, img = cap.read()
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    hands_detector = hands.process(img_rgb)
-    face_detector = face.process(img_rgb)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    hands_detector = hands.process(imgRGB)
+
     if hands_detector.multi_hand_landmarks:
         for hand in hands_detector.multi_hand_landmarks:
             mp_draw.draw_landmarks(img, hand, mp_hands.HAND_CONNECTIONS)
-    if face_detector.multi_face_landmarks:
-        for face in face_detector.multi_face_landmarks:
-            mp_draw.draw_landmarks(img, face, mp_face.FACEMESH_TESSELATION)
-            mp_draw.draw_landmarks(img, face, mp_face.FACEMESH_CONTOURS)
-            mp_draw.draw_landmarks(img, face, mp_face.FACEMESH_IRISES)
+
+    img.flags.writeable = False
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = face.process(img)
+
+    img.flags.writeable = True
+    img= cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            mp_draw.draw_landmarks(image=img, landmark_list=face_landmarks, connections=mp_face.FACEMESH_TESSELATION, landmark_drawing_spec=None)
+            mp_draw.draw_landmarks(image=img, landmark_list=face_landmarks, connections=mp_face.FACEMESH_CONTOURS, landmark_drawing_spec=None)
+            mp_draw.draw_landmarks(image=img, landmark_list=face_landmarks, connections=mp_face.FACEMESH_IRISES, landmark_drawing_spec=None)
 
     cv2.imshow("Detector", img)
     cv2.waitKey(1)
